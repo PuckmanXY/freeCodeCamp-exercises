a1968a3e704f87e5ca08ae0afb1ee18fc9341292
PuckmanXY <kayoanderson0403@gmail.com>
2018-04-09 10:11:24 -0300

    title-case solved

--- basic-algorithm-scripting/title-case.js
+++ basic-algorithm-scripting/title-case.js
@@ -3,9 +3,8 @@ function titleCase(str) {
     var splited = str.split(' ');
     str = '';
     for (var i = 0; i < splited.length; i++) {
-        splited[i].toLowerCase();
-        splited[i][0] = splited[i].charAt(0).toUpperCase();
-        console.log(splited[i][0]);
+        splited[i] = splited[i].toLowerCase();
+        splited[i] = splited[i].replace(splited[i][0], splited[i].charAt(0).toUpperCase());
         if (i == splited.length - 1)
             str += splited[i];
         else
